4cba17e611b1b3376409ab4721b7214d4c19ce63
martinafarah6 <martinafarah6@gmail.com>
2026-09-15 15:45:59 +0300

    Add index.js to export multiple components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Header} from './Header/Header';
+export {default as Home} from './Home/Home';
+export {default as Login} from './Login/Login';
+export {default as Movie} from './Movie/Movie';
+export {default as Favorites} from './Favorites/Favorites';
+export {default as Search} from './Search/Search';
+export {default as Contact} from './Contact/Contact';
+export {default as Footer} from './Footer/Footer';
